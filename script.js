efcfb115efc9b1416660cb216439e46e4471b657
Ayush Bansal <ayushbansal1011@gmail.com>
2023-10-09 22:45:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+console.log(buttons);
+
+buttons.forEach((ele)=>{
+    console.log(ele);
+    ele.addEventListener('click',function(e){
+        if (e.target.id==='grey'){
+            body.style.backgroundColor='grey'}
+        if (e.target.id==='white'){
+            body.style.backgroundColor='white'}
+        if (e.target.id==='blue'){
+            body.style.backgroundColor='blue'}
+        if (e.target.id==='yellow'){
+            body.style.backgroundColor='yellow'}
+        
+        
+    })
+
+
+})
+
+
